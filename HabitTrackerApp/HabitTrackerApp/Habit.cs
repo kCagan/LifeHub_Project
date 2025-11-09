128001d28a685d8638d696f7af557b5b925e79d7
kCagan <caganakin41@gmail.com>
2025-11-10 01:19:16 +0300

    newNamings
    newNamings made for xaml files

--- HabitTrackerApp/HabitTrackerApp/Habit.cs
+++ HabitTrackerApp/HabitTrackerApp/Habit.cs
@@ -2,7 +2,7 @@ namespace HabitTrackerApp;
 
 public class HabitModel
 {
-    public string? Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public DateTime DateAdded { get; set; }
     public bool IsCompleted { get; set; }
 }
